53c3e11b0b45680f79b5c550812046f46be8c120
hakonamatata177 <haakonfar@proton.me>
2026-05-27 12:21:28 +0200

    Create keys_no.h for Norwegian keyboard layout
    Added Norwegian key definitions for keyboard layout.

---
+++ config/keys_no.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <dt-bindings/zmk/keys.h>
+
+// Norwegian key definitions
+// Based on Norwegian keyboard layout (NO)
+
+#define NO_AA  LBKT        // å
+#define NO_AE  SQT         // æ
+#define NO_OE  SEMI        // ø
+
+#define NO_AA_UPPER  LS(LBKT)   // Å
+#define NO_AE_UPPER  LS(SQT)    // Æ
+#define NO_OE_UPPER  LS(SEMI)   // Ø
